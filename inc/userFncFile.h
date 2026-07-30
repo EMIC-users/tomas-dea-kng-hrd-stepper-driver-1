08c032196ce8af23b13b37a0728e351968024f8f
EMIC Compiler <emic-compiler@emic.io>
2026-07-30 23:27:19 +0000

    Code generation: 2026-07-30 23:27:19 UTC
    Errors: 0
    Warnings: 0
    
    ---
    Module: hrd-stepper-driver-1
    Project: dea-kng
    User: tomas

--- inc/userFncFile.h
+++ inc/userFncFile.h
@@ -15,6 +15,7 @@
 void onReset();
 void etOut1();
 void StepperDriver_Motor_onLimitSwitch();
+void eI2C(char* tag, const streamIn_t* const msg);
 
 #endif /* USERFNCFILE_H */
 
--- userFncFile.c
+++ userFncFile.c
@@ -19,42 +19,16 @@ void onReset()
 {
     LEDs_Led1_state(1);
     pI2C("BOOT\t1");
-    setTime1(3000, 'A');
+    setTime1(1500, 'T');
 }
 
 
 void etOut1()
 {
-    seq = seq + 1;
-    switch (seq)
-    {
-        case 1:
-            pI2C("SEQ\t1-cfg");
-            StepperDriver_Motor_sleep(0);
-            StepperDriver_Motor_setMicrostep(8);
-            StepperDriver_Motor_setSpeed(150);
-            break;
-        case 2:
-            pI2C("SEQ\t2-abre400");
-            StepperDriver_Motor_move(1, 400);
-            break;
-        case 3:
-            pI2C("SEQ\t3-cierra400");
-            StepperDriver_Motor_move(0, 400);
-            break;
-        case 4:
-            pI2C("SEQ\t4-abre200");
-            StepperDriver_Motor_move(1, 200);
-            break;
-        case 5:
-            pI2C("SEQ\t5-cierra200");
-            StepperDriver_Motor_move(0, 200);
-            break;
-        case 6:
-            pI2C("SEQ\t6-fin");
-            setTime1(0, 'T');
-            break;
-    }
+    StepperDriver_Motor_sleep(0);
+    StepperDriver_Motor_setMicrostep(8);
+    StepperDriver_Motor_setSpeed(150);
+    pI2C("LISTO\t1");
 }
 
 
@@ -62,6 +36,36 @@ void StepperDriver_Motor_onLimitSwitch()
 {
     LEDs_Led2_state(1);
     pI2C("HOME\t1");
+}
+
+
+void eI2C(char* tag, const streamIn_t* const msg)
+{
+    if (strncmp(tag, "ABRIR", 5) == 0)
+    {
+        paso = streamIn_t_ptr_to_uint16_t((streamIn_t*)msg);
+        pI2C("PREA\t%u", paso);
+        StepperDriver_Motor_move(1, paso);
+        pI2C("POSA\t%u", paso);
+    }
+    else if (strncmp(tag, "CERRAR", 6) == 0)
+    {
+        paso = streamIn_t_ptr_to_uint16_t((streamIn_t*)msg);
+        pI2C("PREC\t%u", paso);
+        StepperDriver_Motor_move(0, paso);
+        pI2C("POSC\t%u", paso);
+    }
+    else if (strncmp(tag, "IRA", 3) == 0)
+    {
+        paso = streamIn_t_ptr_to_uint16_t((streamIn_t*)msg);
+        pI2C("PREI\t%u", paso);
+        StepperDriver_Motor_goTo(paso);
+        pI2C("POSI\t%u", paso);
+    }
+    else
+    {
+        /* default case - no action */
+    }
 }
 
 
