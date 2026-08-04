8e8b2281b743402237e610c7846513a315223517
EMIC Compiler <emic-compiler@emic.io>
2026-08-04 12:32:09 +0000

    Code generation: 2026-08-04 12:32:09 UTC
    Errors: 0
    Warnings: 0
    
    ---
    Module: hrd-stepper-driver-1
    Project: dea-kng
    User: tomas

--- inc/StepperDriver_Motor.h
+++ inc/StepperDriver_Motor.h
@@ -29,9 +29,7 @@ void StepperDriver_Motor_move(uint8_t direction, uint16_t steps);
 
 void StepperDriver_Motor_sleep(uint8_t state);
 
-void StepperDriver_Motor_goHome(uint8_t direction);
 
-void StepperDriver_Motor_goTo(uint32_t position);
 
 /*==================[events]================================================*/
 
--- userFncFile.c
+++ userFncFile.c
@@ -12,8 +12,11 @@
 #include "inc/EMICBus.h"
 
 /* User Variables */
-uint8_t seq = 0;
-uint16_t paso = 0;
+uint8_t fase = 0;
+uint16_t paAct = 0;
+uint16_t paRef = 0;
+uint16_t cnt = 0;
+uint8_t movia = 0;
 
 void onReset()
 {
@@ -25,45 +28,102 @@ void onReset()
 
 void etOut1()
 {
-    StepperDriver_Motor_sleep(0);
-    StepperDriver_Motor_setMicrostep(8);
-    StepperDriver_Motor_setSpeed(150);
-    StepperDriver_Motor_goHome(0);
-    pI2C("LISTO\t1");
+    switch ((uint8_t)(fase))
+    {
+        case 0:
+            StepperDriver_Motor_sleep(0);
+            StepperDriver_Motor_setMicrostep(2);
+            StepperDriver_Motor_setSpeed(100);
+            pI2C("PACONF\t1");
+            fase = 1;
+            cnt = 0;
+            setTime1(100, 'A');
+            break;
+        case 1:
+            StepperDriver_Motor_move(0, 1);
+            cnt = cnt + 1;
+            if (cnt >= 200)
+            {
+                fase = 4;
+                pI2C("PAERR\tnohome");
+            }
+            break;
+        case 2:
+            StepperDriver_Motor_move(1, 1);
+            cnt = cnt + 1;
+            if (cnt >= 10)
+            {
+                paAct = 0;
+                paRef = 0;
+                movia = 0;
+                fase = 3;
+                pI2C("PAPOS\t0");
+            }
+            break;
+        case 3:
+            if (paAct < paRef)
+            {
+                StepperDriver_Motor_move(1, 1);
+                paAct = paAct + 1;
+                movia = 1;
+            }
+            else
+            {
+                if (paAct > paRef)
+                {
+                    StepperDriver_Motor_move(0, 1);
+                    paAct = paAct - 1;
+                    movia = 1;
+                }
+                else
+                {
+                    if (movia == 1)
+                    {
+                        pI2C("PAPOS\t%u", paAct);
+                        movia = 0;
+                    }
+                }
+            }
+            break;
+    }
 }
 
 
 void StepperDriver_Motor_onLimitSwitch()
 {
-    LEDs_Led2_state(1);
-    pI2C("HOME\t1");
+    if (fase == 1)
+    {
+        fase = 2;
+        cnt = 0;
+        LEDs_Led2_state(1);
+        pI2C("PAHOME\t1");
+    }
 }
 
 
 void eI2C(char* tag, const streamIn_t* const msg)
 {
-    if (strncmp(tag, "ABRIR", 5) == 0)
+    if (strncmp(tag, "PAREF", 5) == 0)
     {
-        paso = streamIn_t_ptr_to_uint16_t((streamIn_t*)msg);
-        StepperDriver_Motor_move(1, paso);
-        pI2C("OKABRIR\t%u", paso);
-    }
-    else if (strncmp(tag, "CERRAR", 6) == 0)
-    {
-        paso = streamIn_t_ptr_to_uint16_t((streamIn_t*)msg);
-        StepperDriver_Motor_move(0, paso);
-        pI2C("OKCERRAR\t%u", paso);
-    }
-    else if (strncmp(tag, "IRA", 3) == 0)
-    {
-        paso = streamIn_t_ptr_to_uint16_t((streamIn_t*)msg);
-        StepperDriver_Motor_goTo(paso);
-        pI2C("OKIRA\t%u", paso);
+        paRef = streamIn_t_ptr_to_uint16_t((streamIn_t*)msg);
+        if (paRef > 100)
+        {
+            paRef = 100;
+        }
     }
     else if (strncmp(tag, "HOMEAR", 6) == 0)
     {
-        StepperDriver_Motor_goHome(0);
-        pI2C("OKHOME\t1");
+        LEDs_Led2_state(0);
+        cnt = 0;
+        fase = 1;
+    }
+    else if (strncmp(tag, "ESTOP", 5) == 0)
+    {
+        paRef = 0;
+    }
+    else if (strncmp(tag, "FALLO", 5) == 0)
+    {
+        paRef = 0;
     }
     else
     {
