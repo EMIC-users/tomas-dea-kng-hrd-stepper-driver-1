6d63b69d835dfabb8495f418eb833163d7b11e7c
EMIC Compiler <emic-compiler@emic.io>
2026-08-04 15:20:25 +0000

    Code generation: 2026-08-04 15:20:25 UTC
    Errors: 0
    Warnings: 0
    
    ---
    Module: hrd-stepper-driver-1
    Project: dea-kng
    User: tomas

--- userFncFile.c
+++ userFncFile.c
@@ -13,8 +13,18 @@
 
 /* User Variables */
 uint16_t pct = 0;
-uint16_t pasos = 0;
+uint16_t pos = 0;
 uint8_t cfg = 0;
+uint8_t estado = 0;
+uint16_t rpm = 0;
+uint16_t rpmRef = 600;
+uint16_t banda = 50;
+uint16_t paso = 4;
+uint16_t acc = 0;
+uint8_t n = 0;
+uint16_t prom = 0;
+uint16_t lim1 = 0;
+uint16_t lim2 = 0;
 
 void onReset()
 {
@@ -36,7 +46,7 @@ void etOut1()
     }
     else
     {
-        pI2C("PAPOS\t%u", pct);
+        pI2C("PAPOS\t%u", pos);
     }
 }
 
@@ -45,7 +55,7 @@ void StepperDriver_Motor_onLimitSwitch()
 {
     StepperDriver_Motor_goTo(300);
     LEDs_Led2_state(1);
-    pct = 37;
+    pos = 300;
     pI2C("PAHOME\t1");
     setTime1(2000, 'A');
 }
@@ -60,8 +70,75 @@ void eI2C(char* tag, const streamIn_t* const msg)
         {
             pct = 100;
         }
-        pasos = pct * 8;
-        StepperDriver_Motor_goTo(pasos);
+        pos = pct * 8;
+        StepperDriver_Motor_goTo(pos);
+    }
+    else if (strncmp(tag, "PASTEP", 6) == 0)
+    {
+        pos = streamIn_t_ptr_to_uint16_t((streamIn_t*)msg);
+        if (pos > 800)
+        {
+            pos = 800;
+        }
+        StepperDriver_Motor_goTo(pos);
+    }
+    else if (strncmp(tag, "RPMREF", 6) == 0)
+    {
+        rpmRef = streamIn_t_ptr_to_uint16_t((streamIn_t*)msg);
+    }
+    else if (strncmp(tag, "RPMBAND", 7) == 0)
+    {
+        banda = streamIn_t_ptr_to_uint16_t((streamIn_t*)msg);
+    }
+    else if (strncmp(tag, "PASO", 4) == 0)
+    {
+        paso = streamIn_t_ptr_to_uint16_t((streamIn_t*)msg);
+    }
+    else if (strncmp(tag, "ESTADO", 6) == 0)
+    {
+        estado = streamIn_t_ptr_to_uint8_t((streamIn_t*)msg);
+    }
+    else if (strncmp(tag, "RPMMOTOR", 8) == 0)
+    {
+        rpm = streamIn_t_ptr_to_uint16_t((streamIn_t*)msg);
+        if (estado == 2)
+        {
+            acc = acc + rpm;
+            n = (uint8_t)(n + 1);
+            if (n >= 5)
+            {
+                prom = acc / 5;
+                acc = 0;
+                n = 0;
+                lim1 = rpmRef - banda;
+                lim2 = rpmRef + banda;
+                if (prom < lim1)
+                {
+                    pos = pos + paso;
+                    if (pos > 800)
+                    {
+                        pos = 800;
+                    }
+                    StepperDriver_Motor_goTo(pos);
+                }
+                if (prom > lim2)
+                {
+                    if (pos > paso)
+                    {
+                        pos = pos - paso;
+                    }
+                    else
+                    {
+                        pos = 8;
+                    }
+                    if (pos < 8)
+                    {
+                        pos = 8;
+                    }
+                    StepperDriver_Motor_goTo(pos);
+                }
+            }
+        }
     }
     else
     {
